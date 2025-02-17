bec84737fd428a6bcb1e5437779b0b466e23a4cc
peter-itsup <hanshansen33011@gmail.com>
2025-02-17 10:02:48 +0100

    All new files

---
+++ day1-exercises.ps1
@@ -0,0 +1,12 @@
+# Opgave 1: Opret en mappe i hjemmekataloget 
+# Uncomment below this line to execute task
+
+# New-Item -Itemtype Directory -Path ~/Documents/IT-SUP/h2/server-aut/powershell-labs/new_directory
+#-----------------------------------------------------------
+
+# Opgave 2: Få en liste over kørende tjenester
+
+# Get-Process
+# ----------------------------------------------------------
+
+
